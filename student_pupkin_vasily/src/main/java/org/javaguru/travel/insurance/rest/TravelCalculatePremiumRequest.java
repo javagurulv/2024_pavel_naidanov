df60899e0893f6a6287f1abe7ff9cd9b47ce05f2
Pupkin <frd-ru@yandex.ru>
2024-02-14 12:37:12 +0300

    STEP 002 - DONE

--- student_pupkin_vasily/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumRequest.java
+++ student_pupkin_vasily/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumRequest.java
@@ -1,7 +1,16 @@
 package org.javaguru.travel.insurance.rest;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class TravelCalculatePremiumRequest {
 
     private String personFirstName;
