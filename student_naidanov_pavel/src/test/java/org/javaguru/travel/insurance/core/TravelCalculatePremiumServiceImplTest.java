79823161455e1279e5bb6afe61c45bd8ad75c00e
Nifpd <naidanov_p@mail.ru>
2024-02-20 19:07:14 +0500

    STEP 005 - DONE

--- student_naidanov_pavel/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_naidanov_pavel/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -1,12 +1,23 @@
 package org.javaguru.travel.insurance.core;
 
+import org.assertj.core.api.Assertions;
+import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
+import org.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
 
 class TravelCalculatePremiumServiceImplTest {
 
     @Test
     public void deleteMe() {
+        var travel = new TravelCalculatePremiumServiceImpl();
+        var request = new TravelCalculatePremiumRequest("John", "Doe", new Date(1), new Date(1));
+        var expected = new TravelCalculatePremiumResponse( "John", "Doe", new Date(1), new Date(1));
+        
+        var calculatePremiumResponse = travel.calculatePremium(request);
 
+        Assertions.assertThat(calculatePremiumResponse).isEqualTo(expected);
     }
 
 }
