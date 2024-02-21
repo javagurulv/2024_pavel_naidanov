85a3abb13621c569967504ca172527b6fba8c75b
Nifpd <naidanov_p@mail.ru>
2024-02-21 17:21:58 +0500

    STEP 008 - DONE

--- student_naidanov_pavel/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
+++ student_naidanov_pavel/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
@@ -1,5 +1,6 @@
 package org.javaguru.travel.insurance.rest;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Objects;
 
@@ -33,6 +34,7 @@ public class TravelCalculatePremiumResponse {
     private String personLastName;
     private Date agreementDateFrom;
     private Date agreementDateTo;
+    private BigDecimal agreementPrice;
 
     public TravelCalculatePremiumResponse() {
     }
@@ -80,5 +82,13 @@ public class TravelCalculatePremiumResponse {
 
     public void setAgreementDateTo(Date agreementDateTo) {
         this.agreementDateTo = agreementDateTo;
+    }
+
+    public BigDecimal getAgreementPrice() {
+        return agreementPrice;
+    }
+
+    public void setAgreementPrice(BigDecimal agreementPrice) {
+        this.agreementPrice = agreementPrice;
     }
 }
