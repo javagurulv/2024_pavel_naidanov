c0f0c8575d982faebfd4eb465b371bd928ad7895
Nifpd <naidanov_p@mail.ru>
2024-02-21 17:06:34 +0500

    STEP 006 - DONE

--- student_naidanov_pavel/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_naidanov_pavel/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -10,14 +10,46 @@ import java.util.Date;
 class TravelCalculatePremiumServiceImplTest {
 
     @Test
-    public void deleteMe() {
+    public void should_populate_person_first_name() {
         var travel = new TravelCalculatePremiumServiceImpl();
         var request = new TravelCalculatePremiumRequest("John", "Doe", new Date(1), new Date(1));
         var expected = new TravelCalculatePremiumResponse( "John", "Doe", new Date(1), new Date(1));
         
         var calculatePremiumResponse = travel.calculatePremium(request);
 
-        Assertions.assertThat(calculatePremiumResponse).isEqualTo(expected);
+        Assertions.assertThat(calculatePremiumResponse.getPersonFirstName()).isEqualTo(expected.getPersonFirstName());
     }
 
+    @Test
+    public void should_populate_person_last_name() {
+        var travel = new TravelCalculatePremiumServiceImpl();
+        var request = new TravelCalculatePremiumRequest("John", "Doe", new Date(1), new Date(1));
+        var expected = new TravelCalculatePremiumResponse("John", "Doe", new Date(1), new Date(1));
+
+        var calculatePremiumResponse = travel.calculatePremium(request);
+
+        Assertions.assertThat(calculatePremiumResponse.getPersonLastName()).isEqualTo(expected.getPersonLastName());
+    }
+
+    @Test
+    public void should_populate_agreement_date_from() {
+        var travel = new TravelCalculatePremiumServiceImpl();
+        var request = new TravelCalculatePremiumRequest("John", "Doe", new Date(1), new Date(1));
+        var expected = new TravelCalculatePremiumResponse("John", "Doe", new Date(1), new Date(1));
+
+        var calculatePremiumResponse = travel.calculatePremium(request);
+
+        Assertions.assertThat(calculatePremiumResponse.getAgreementDateFrom()).isEqualTo(expected.getAgreementDateFrom());
+    }
+
+    @Test
+    public void should_populate_agreement_date_to() {
+        var travel = new TravelCalculatePremiumServiceImpl();
+        var request = new TravelCalculatePremiumRequest("John", "Doe", new Date(1), new Date(1));
+        var expected = new TravelCalculatePremiumResponse("John", "Doe", new Date(1), new Date(1));
+
+        var calculatePremiumResponse = travel.calculatePremium(request);
+
+        Assertions.assertThat(calculatePremiumResponse.getAgreementDateTo()).isEqualTo(expected.getAgreementDateTo());
+    }
 }
